b9a6668ce3d85f7d372ce23c695399a35ff4e1c5
Ahmad Dusmatov <dusmatov9999@gmail.com>
2024-07-10 09:30:57 +0500

    refactor and lesson3 test

--- lesson2/test_02.py
+++ lesson2/test_02.py
@@ -1,8 +1,20 @@
 x = 2
-y = 8
-print("x == y", x == y)
-print("x != y", x != y)
-print("x > y", x > y)
-print("x < y", x < y)
-print("x >= y", x >= y)
-print("x <= y", x <= y)
+y = 3
+print("x =", x)
+print("y =", y)
+print("x + y =", x + y)
+print("y - x =", y - x)
+print("x ^ y =", x ** y)
+
+x = 10
+y = 7
+print("x =", x)
+print("y =", y)
+print("x / y =", x / y)
+print("x // y =", x // y)
+print("x % y =", x % y)
+
+print("\n=======\n")
+
+print("aaa", end=" *-* ")
+print("bbb", "ccc", sep=" %%% ")
